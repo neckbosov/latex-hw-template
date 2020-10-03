647f446a43030448112ce27c9ac0f568c4c83e9e
Nikita Bosov <neckbosov@gmail.com>
2020-10-03 19:06:36 +0300

    remove old script

--- r.sh
+++
@@ -1,8 +0,0 @@
-for f in $1; do 
-  name=${f%.*}
-  rm -f $name.{toc,log,out,aux}
-  pdflatex -shell-escape $name.tex || exit
-  pdflatex -shell-escape -interaction=nonstopmode >/dev/null $name.tex || exit
-  rm -f $name.{toc,log,out,aux,dvi,pyg,fls}
-  rm -rf _minted*
-done
